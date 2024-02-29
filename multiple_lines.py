58cd303dbb5410e4a8d1dd96650f8f25e9195c53
Andur909 <147121202+Andur909@users.noreply.github.com>
2024-02-29 11:36:41 -0500

    Added py files with flask

---
+++ multiple_lines.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def main():
+
+    data = "Hello World! <h1>Hello</h1>"
+    data = data + "<h2>Heading 2</h2> <h6>Heading 6</h6> <br> <p>paragraph</p>"
+    return data
+
+if __name__ == "__main__":
+    app.run()
